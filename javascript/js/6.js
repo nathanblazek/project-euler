00eebd697edbf38708259a77a02d00853119a350
Nathan Blazek <ncblazek@gmail.com>
2019-01-12 23:17:21 -0600

    project 6 & 7 done

---
+++ javascript/js/6.js
@@ -0,0 +1,17 @@
+var lesson = {
+    "number":6,
+    "pass":true,
+    "description": "The sum of the squares of the first ten natural numbers is,\n12 + 22 + ... + 102 = 385\nThe square of the sum of the first ten natural numbers is,\n\n(1 + 2 + ... + 10)2 = 552 = 3025\n\nHence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.\n\nFind the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum."
+}
+
+function execute(){
+    var sum = 0;
+    var sumSquared = 0;
+    for(var i=1;i <=100;i++){
+        sum += i;
+        sumSquared += (i * i);
+    }
+    sum =  sum * sum; //square the sum
+    output(sum - sumSquared);
+
+}
---
+++ javascript/js/7.js
@@ -0,0 +1,31 @@
+var lesson = {
+    "number":7,
+    "pass":true,
+    "description": "By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.\n\nWhat is the 10 001st prime number?"
+}
+
+function execute(){
+    var i = 1;
+    var primeCount = 0;
+
+    while(true){
+        if(isPrime(i)){
+            primeCount++;
+        }
+
+        if (primeCount == 10001){
+            output(i);
+            break;
+        }
+        i++;   
+    }
+}
+
+function isPrime(value) {
+    for(var i = 2; i < value; i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+}
